4bdc8a1179370cd2a75657c4de83a4b21ced9889
unknown <bertylliu@gmail.com>
2024-05-01 11:57:25 +0800

    announcement class hash update

--- announcement.py
+++ announcement.py
@@ -1,13 +1,13 @@
-import requests
+import requests, hashlib
 from bs4 import BeautifulSoup as BS 
 class Announcement():
     def __init__(self, link : str) -> None:
         r : requests.Response = requests.get(link)
         soup : BS = BS(r.text, "html.parser")
-        self.news = soup.find("table", class_ = "single_news")
-        self.hash_value = hash(link)
-
         self.link : str = link
+        self.news = soup.find("table", class_ = "single_news")
+        self.hash_value = self.__get_hash()
+
         self.title : str = self.news.find("tr", class_ = "news_title").td.string
         self.date : str = self.news.find("tr", class_ = "news_date").td.string
         self.unit : str = self.news.find("tr", class_ = "news_unit").td.string
@@ -15,6 +15,11 @@ class Announcement():
         self.content : list[str] = self.__get_content()
         self.attachment : list[tuple[str, str]] = self.__get_attachment() # list pair -> (name, link)
 
+    def __get_hash(self) -> int:
+        data = hashlib.md5()
+        data.update(self.link.encode('utf-8'))
+        # print(type(data.digest()))
+        return int.from_bytes(data.digest())
     def __get_content(self) -> str:
         contents = self.news.find("tr", class_ = "news_content").td.div
         content_temp = []
@@ -29,3 +34,4 @@ class Announcement():
             link = attach_tag.find("span", class_ = "modal_download").a.get("href")
             attach_pairs.append((name, link))
         return attach_pairs
+    
--- check.py
+++ check.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup as BS
 from announcement import Announcement
 ann_link = "https://www.ttsh.tp.edu.tw/category/news/news_01/"
 while True:
-    r = requests.get("https://www.ttsh.tp.edu.tw/category/news/news_01/")
+    r = requests.get(ann_link)
     soup = BS(r.text,"html.parser")
     announcements_tag = soup.find_all("a", class_ = "news_title", limit=20)
     
